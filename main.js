f8a5e64cbb5ebcc696aead56b4a741681438c3bc
Parveen Shaikh <shparmeen@gmail.com>
2020-02-06 19:29:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-var express    = require("express");
-var apis 	   = require('./routes/loginroutes');
-var bodyParser = require('body-parser');
+const express    = require("express");
+const apis 	   = require('./routes/loginroutes');
+const bodyParser = require('body-parser');
+const session	   = require('express-session');
+const path 	   = require('path');
 
-var app = express();
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-var router = express.Router();
+const router = express.Router();
 
 // test route
-router.get('/', function(req, res) {
+/*router.get('/', function(req, res) {
     res.json({ message: 'welcome to our upload module apis' });
+});*/
+
+router.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 //route to handle user registration
 router.post('/register',apis.register);
@@ -30,4 +44,4 @@ router.get('/planetVehiclePilotsDetail',apis.planetVehiclePilotsDetail);
 
 
 app.use('/api', router);
-app.listen(9002);
+app.listen(9003);
